508aae764ac186383af6eb7f3c40de506227c868
Nithulya K Ramachandran <nithulya91@gmail.com>
2024-03-31 21:31:20 -0500

    String programs in SQA Tool

--- Nithulya/Python Basic Programs/Python loops programs.py
+++ Nithulya/Python Basic Programs/Python loops programs.py
@@ -423,28 +423,28 @@
 for i in range(1,2):
     for j in range(1,26):
         if j==1 or j==7 or j==9 or j==11 or j==12 or j==13 or j==14 or j==15 or j==17 or j==18 or j==21 or j==25:
-            print('*', end=' ')
+            print('#', end=' ')
         else:
             print(' ', end=' ')
 print()
 for i in range(1,2):
     for j in range(1,26):
         if j==2 or j==6 or j==9 or j==13 or j==16 or j==19 or j==22 or j==24:
-            print('*', end=' ')
+            print('#', end=' ')
         else:
             print(' ', end=' ')
 print()
 for i in range(1,2):
     for j in range(1,26):
         if j==3 or j==5 or j==9 or j==11 or j==13 or j==16 or j==17 or j==18 or j==19 or j==23:
-            print('*', end=' ')
+            print('#', end=' ')
         else:
             print(' ', end=' ')
 print()
 for i in range(1,2):
     for j in range(1,26):
         if j==4 or j==9 or j==11 or j==12 or j==13 or j==16 or j==19 or j==23:
-            print('*', end=' ')
+            print('#', end=' ')
         else:
             print(' ', end=' ')
 print()
