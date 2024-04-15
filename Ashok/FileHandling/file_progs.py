03a24bf163a27e7f074be0cc69539d557b589fa3
ashokpusarla002 <ashok.pusarla002@gmail.com>
2024-04-15 23:55:02 +0530

    FileHandling

---
+++ Ashok/FileHandling/file_progs.py
@@ -0,0 +1,40 @@
+# Write a python program to combine both the files with alternate lines and create new file.
+
+def combine_two_files(file1, file2, file3):
+    with open(file1, "r") as file1_obj:
+        file1_list = file1_obj.readlines()
+
+    with open(file2, "r") as file2_obj:
+        file2_list = file2_obj.readlines()
+
+    file3_list = []
+    len_list = 0
+    if len(file1_list) > len(file2_list):
+        len_list = len(file1_list)
+    else:
+        len_list = len(file2_list)
+
+    for i in range(len_list):
+        file3_list.append(file1_list[i])
+        file3_list.append(file2_list[i])
+
+    with open(file3, "a") as file3_obj:
+        for line in file3_list:
+            file3_obj.write(line)
+
+
+combine_two_files("read_file1.txt", "read_file2.txt", "append_file3.txt")
+
+# Homework :
+# 1. write a python program Replace file content JAVA with PYTHON.
+# 2. write a python program to get all the email id from given file.
+# 3. Write a python program to get all the phone number from given file.
+
+# Python program to replace text in a file
+s = input("Enter text to replace the existing contents:")
+m = input("Enter text to replace the existing contents:")
+f = open("file.txt", "r+")  # r+ mode opens the file in read and write mode
+f.replace(s)
+f.write(m)
+f.close()
+print("Text successfully replaced")
---
+++ Ashok/FileHandling/filehandling.py
@@ -0,0 +1,147 @@
+"""
+The key function for working with files in Python is the open() function.
+The open() function takes two parameters; filename, and mode. # syntax: f=open("filename.txt","filemode")
+To open the file, use the built-in open() function.
+The open() function returns a file object, which has a read() method for reading the content of the file:
+
+There are four different methods (modes) for opening a file:
+"r" - Read - Default value. Opens a file for reading, error if the file does not exist
+"a" - Append - Opens a file for appending the content to end of existing data, or creates the file
+      if it does not exist
+"w" - Write - Opens a file for writing and overrides the existing content if file exist, or creates the file
+       if it does not exist
+"x" - Create - Creates the specified file, returns an error if the file exists
+
+In addition, you can specify if the file should be handled as binary or text mode
+"t" - Text - Default value. Text mode
+"b" - Binary - Binary mode (e.g. images)
+"""
+
+
+def read_file(filename):
+    file_reading = open(filename, "r")
+    printing_file = file_reading.read()
+    print(printing_file)
+    file_reading.close()
+
+
+read_file("C:\\PythonAutomation\\GTM_PS_Batch03\\Ashok\\FileHandling\\demo1.txt")
+
+
+def write_append_file1(filename, content):
+    file_writing = open(filename, "a")
+    file_writing.write(content)
+    file_writing.close()
+
+
+write_append_file1("demo1.txt", "\n9. hello world")
+write_append_file1("demo2.txt", "1. Hello world, this is a new file")
+
+
+def write_writing_file2(filename, content):
+    file_writing = open(filename, "w")
+    file_writing.write(content)
+    file_writing.close()
+
+
+write_writing_file2("demo2.txt", "Hello Ashok, replacing with previous content")
+write_writing_file2("demo3_new.txt", "Hello This is a new file")
+
+
+# Context Manager #
+def read_file_with_context_manager(filename):
+    with open(filename, "r") as file_obj:
+        data = file_obj.read()
+        print(data)
+        print("file is closed :", file_obj.closed)
+
+    print("file is closed :", file_obj.closed)
+
+
+read_file_with_context_manager("demo1.txt")
+
+
+def write_file_with_context_manager(filename, content):
+    with open(filename, "a") as file_obj2:
+        file_obj2.write(content)
+        print("file is closed :", file_obj2.closed)
+
+    print("file is closed :", file_obj2.closed)
+
+
+write_file_with_context_manager("demo4_new.txt", "This a file with context manager")
+
+
+# Different read options of the file #
+# 1. Read a specific number of characters from file
+
+def read_number_of_bytes(filename, byte_count):
+    with open(filename, "r") as file_obj3:
+        data1 = file_obj3.read(byte_count)
+        print(data1)
+
+
+read_number_of_bytes("demo1.txt", 30)
+
+
+# 2. Read n number of line with readline method. #
+def read_number_of_lines(filename, lines_count):
+    with open(filename, "r") as file_obj4:
+        for i in range(lines_count):
+            data = file_obj4.readline()
+            print(data, end="")
+
+
+read_number_of_lines("demo1.txt", 5)
+
+
+# 3. read list of lines with help of readlines #
+def read_list_of_lines(filename):
+    with open(filename, "r") as file:
+        lines_data = file.readlines()
+        return lines_data
+
+
+print(read_list_of_lines("demo1.txt"))
+print()
+
+
+# 4. read a specific line from list #
+def read_specific_line(filename, line_no):
+    lines_list = read_list_of_lines(filename)
+    print(lines_list[line_no - 1])
+
+
+read_specific_line("demo1.txt", 5)
+
+"""
+# tell() method : This method will tell you the current cursor position.
+# seek method :  This method will help to set the cursor position with respect to
+# 1. Begining of file
+# 2. End of file
+# 3. current position of cursor
+"""
+
+
+def tell_different_cursor_position(filename):
+    with open(filename, "rb") as file:
+        print("start position of cursor :", file.tell())
+        char_10 = file.read(10)
+        print(char_10)
+        print("current position of cursor :", file.tell())
+
+
+tell_different_cursor_position("demo1.txt")
+
+
+def set_different_cursor_position(filename):
+    with open(filename, "rb") as file:
+        # set cursor at 10 position from begining of file
+        # file.seek(10, 0)  # setting the cursor position from begining of file
+        # file.seek(10, 1)  # set the cursor position from current position of the cursor
+        file.seek(-30, 2)  # set the cursor position from end of the file
+        print("start position of cursor :", file.tell())
+        print(file.read())
+
+
+set_different_cursor_position("demo1.txt")
