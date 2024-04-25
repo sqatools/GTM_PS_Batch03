9dad08b367d188c0140513539cdb1cdb05df4b52
Nithulya K Ramachandran <nithulya91@gmail.com>
2024-04-24 21:36:00 -0500

    OOPS Practice

--- Thushara/OopsConcepts/oops_concepts.py
+++ Thushara/OopsConcepts/oops_concepts.py
@@ -22,50 +22,85 @@ data hiding
 abstraction
 """
 
+#1).
+# class ABC:
+#
+#     Team = "CSK" # class variable
+#
+#     def __init__(self, num1, num2):
+#         print("objected initiated")
+#         self.x = num1  # instance variable
+#         self.y = num2  # instance variable
+#
+#     # instance method
+#     def greeting(self):
+#         print("Welcome to OOPS concept")
+#
+#     # instance method
+#     def addition(self):
+#         print("Addition :", self.x + self.y)
+#
+#     def multiplication(self, n1, n2):
+#         print("multiplication :", n1*n2)
+#
+#     def print_ipl_team_name(self):
+#         print("IPL Team name :", self.Team)
+#
+#
+#     @classmethod
+#     def show_class_variable(cls):
+#         print("class variable value :", cls.Team)
+#
+#
+# obj = ABC(30, 40) # creating the object of the class
+# obj.greeting()
+# obj.addition()
+# obj.multiplication(5, 6)
+# print("Team name :", obj.Team)
+#
+# obj.print_ipl_team_name()
+# obj.Team = "Mumbai"
+#
+# obj.print_ipl_team_name()
+#
+# print("_"*50)
+# obj2 = ABC(350, 400)
+# obj2.print_ipl_team_name()
+# obj2.Team = "RCB"
+# obj2.print_ipl_team_name()
+# obj2.show_class_variable()
 
-class ABC:
 
-    Team = "CSK" # class variable
+#2).
+# class Car:
+#     company ='BMW'
+#     model='M5'
+#     price=2500000000
+#     color ='Black'
+#     def displayDetails(self):
+#         print("Printing all details")
+# obj1 = Car()
+# print('Company :',Car.company)
+# print('Model :',Car.model)
+# print('Price :',Car.price)
+# print('Color :',Car.color)
+# obj1.displayDetails()
 
-    def __init__(self, num1, num2):
-        print("objected initiated")
-        self.x = num1  # instance variable
-        self.y = num2  # instance variable
+#3).
+class Employee:
+    company='ABCD Technologies'
+    location='Kerala'
+    def __init__(self,id,name,salary):
+        self.empID = id
+        self.empName=name
+        self.empSalary=salary
+    def details(self,location):
+        self.location1=location
+        print('Good Morning!!',self.empName)
+obj1=Employee('AB123','NIVYA',25000)
+print('Employee ID :',obj1.empID,',','Employee Name : ',obj1.empName,',','Employee Salary : ',obj1.empSalary)
+print('Company :',Employee.company)
+print('Location :',Employee.location)
+obj1.details('Kannur')
+obj2= Employee('CD123','Arya',20000)
 
-    # instance method
-    def greeting(self):
-        print("Welcome to OOPS concept")
-
-    # instance method
-    def addition(self):
-        print("Addition :", self.x + self.y)
-
-    def multiplication(self, n1, n2):
-        print("multiplication :", n1*n2)
-
-    def print_ipl_team_name(self):
-        print("IPL Team name :", self.Team)
-
-
-    @classmethod
-    def show_class_variable(cls):
-        print("class variable value :", cls.Team)
-
-
-obj = ABC(30, 40) # creating the object of the class
-obj.greeting()
-obj.addition()
-obj.multiplication(5, 6)
-print("Team name :", obj.Team)
-
-obj.print_ipl_team_name()
-obj.Team = "Mumbai"
-
-obj.print_ipl_team_name()
-
-print("_"*50)
-obj2 = ABC(350, 400)
-obj2.print_ipl_team_name()
-obj2.Team = "RCB"
-obj2.print_ipl_team_name()
-obj2.show_class_variable()
