8237d2c9124bd78d383ebc021ec9269460c1e5dc
thushara-anwin <thusharaanwin@gmail.com>
2024-05-07 22:05:05 -0500

    Adding project

--- Thushara/Assignments_batch_03/xpath_advanced/yatra_xpath_advanced.py
+++ Thushara/Assignments_batch_03/xpath_advanced/yatra_xpath_advanced.py
@@ -8,6 +8,8 @@ driver.implicitly_wait(10)
 
 driver.get("https://www.yatra.com/")
 search_flights = driver.find_element(By.XPATH,"//input[@value='Search Flights' and @id='BE_flight_flsearch_btn']")
+a =search_flights.text
+print(a)
 search_flights.click()
 
 time.sleep(10)
