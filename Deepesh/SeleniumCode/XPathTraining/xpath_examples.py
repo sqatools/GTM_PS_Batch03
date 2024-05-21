833d5f52a6c228173fdcb37de1bb2fd9a2358304
Tejashree <tejashreebannure@gmail.com>
2024-05-22 00:20:05 +0530

    new file1

--- Deepesh/SeleniumCode/XPathTraining/xpath_examples.py
+++ Deepesh/SeleniumCode/XPathTraining/xpath_examples.py
@@ -19,6 +19,7 @@ password.send_keys("Admin@12345")
 login_button = driver.find_element(By.XPATH, "//button[@data-testid='royal_login_button']")
 login_button.click()
 
+
 incorrect_msg = driver.find_element(By.XPATH, """//div[text()="The password that you've entered is incorrect. "]""")
 print(incorrect_msg.text)
 assert "incorrect" in incorrect_msg.text
