013d2c818d8082c15c7d35c8ba0c2054bc6b1b76
thushara-anwin <thusharaanwin@gmail.com>
2024-05-22 17:20:35 -0500

    pytest frame work

---
+++ Thushara/PytestFramework/test/test_fixture_file2.py
@@ -0,0 +1,35 @@
+"""
+Fixture scope:
+
+function scope = function level fixture will execute for each of the test function in the file.
+module scope = module level fixture will execute once for each module file
+package scope = package level fixture will execute for each of the folder which contains module files.
+session scope = session level fixture has wider scope which will initiate with session of automation execution
+class scope = class level fixture will execute for specific class only.
+
+"""
+import pytest
+
+@pytest.mark.smoke
+def test_addition_file2():
+    var1 = 10
+    var2 = 20
+    assert var1 + var2 == 30
+    print("test addition successful")
+
+
+@pytest.mark.sanity
+@pytest.mark.regression
+def test_multiplication_file2():
+    var1 = 10
+    var2 = 20
+    assert var1 * var2 == 200
+    print("test multiplication successful")
+
+
+@pytest.mark.regression
+def test_subtraction_file2():
+    var1 = 10
+    var2 = 200
+    assert var2 - var1 == 300
+    print("test subtraction successful")
