7b7c0b21889d928bfbd11720f0169ab099a5208c
Nithulya K Ramachandran <nithulya91@gmail.com>
2024-05-23 22:46:02 -0500

    pytestFramework practices

---
+++ Nithulya/SeleniumCode/PytestFrameWork/frameworkPractice2/test_Register.py
@@ -0,0 +1,7 @@
+
+
+def test_register_with_mandatory_field():
+    print('Testing test_register_with_mandatory_fields ')
+
+def test_register_with_all_field():
+    print('Testing test_register_with_all_fields ')
