bab788d28c96f441a26abc098d6d407a35ac19e4
thushara-anwin <thusharaanwin@gmail.com>
2024-05-27 12:29:11 -0500

    pytest-grouping tests in classes

--- Thushara/_MiniProjectsPytestFramework/magento_pytest/conftest.py
+++ Thushara/_MiniProjectsPytestFramework/magento_pytest/conftest.py
@@ -1,10 +1,15 @@
+
+import pytest
 from selenium import webdriver
 
-
-def set_up_and_tear_down():
+@pytest.fixture()
+def set_up_and_tear_down(request):
     driver = webdriver.Chrome()
+    driver.get("https://magento.softwaretestingboard.com/")
     driver.maximize_window()
     driver.implicitly_wait(10)
-    driver.get("https://magento.softwaretestingboard.com/")
+    request.cls.driver=driver
     yield
     driver.close()
+
+
