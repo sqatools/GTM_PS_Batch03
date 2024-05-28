74a980a6d2e2bcb42e3416df9a8f21d240e478b4
Bi12Python <xovek11875@bitvoo.com>
2024-05-28 10:09:59 +0530

    Framework Design

---
+++ Deepesh/SeleniumCode/Mini_Framework_POM/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+from test_data import *
+
+
+@pytest.fixture(scope="class")
+def get_driver(request):
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get(URL)
+    request.cls.driver = driver
