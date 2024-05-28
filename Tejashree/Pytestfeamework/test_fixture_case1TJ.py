5dbc587ccd1a67ae3a218b506e4c5a8998050c28
Tejashree <tejashreebannure@gmail.com>
2024-05-28 23:12:50 +0530

    new file3

--- Tejashree/Pytestfeamework/test_fixture_case1TJ.py
+++ Tejashree/Pytestfeamework/test_fixture_case1TJ.py
@@ -8,4 +8,55 @@ session scope = session level fixture has wider scope which will initiate with s
 class scope = class level fixture will execute for specific class only.
 
 """
-import pytes
+import pytest
+
+@pytest.fixture(scope='function', autouse=True)
+def setup():
+    print("function execution initiated")
+    yield
+    print("function execution completed")
+
+@pytest.fixture(scope='module', autouse=True)
+def setup_module():
+    print("module execution initiated")
+    yield
+    print("module execution completed")
+
+@pytest.fixture(scope='session', autouse=True)
+def setup_session():
+    print("session execution initiated")
+    yield
+    print("session execution completed")
+
+@pytest.fixture(scope='package', autouse=True)
+def setup_package():
+    print("Package Execution initiated \n")
+    yield
+    print("\n Package Execution completed \n")
+
+def test_addition():
+    var1 = 10
+    var2 = 20
+    assert var1 + var2 == 30
+    print("test addition successful")
+
+
+def test_multiplication():
+    var1 = 10
+    var2 = 20
+    assert var1 * var2 == 200
+    print("test multiplication successful")
+
+
+def test_subtraction():
+    var1 = 10
+    var2 = 200
+    assert var2 - var1 == 300
+    print("test subtraction successful")
+
+
+def division():
+    var1 = 10
+    var2 = 20
+    assert var1 // var2 == 30
+
