37b5b4b5d3426225b61e56e20527bd30dab810cc
Bi12Python <xovek11875@bitvoo.com>
2024-06-03 09:46:36 +0530

    Restructuring the code

--- Thushara/_AutomationFramework/tests/login/test_login.py
+++ Thushara/_AutomationFramework/tests/login/test_login.py
@@ -15,47 +15,42 @@ class TestLogin:
         self.lp=LoginPage(self.driver)
         self.re = RandomEmail()
 
-    """
     def test_login_with_valid_credential(self):
-        lp = LoginPage(self.driver)
-        lp.click_sign_in()
-        lp.enter_value_to_email_field(valid_email)
-        lp.enter_value_to_password_field(valid_password)
-        lp.click_sign_in_button()
+        self.lp.click_sign_in()
+        self.lp.enter_value_to_email_field(valid_email)
+        self.lp.enter_value_to_password_field(valid_password)
+        self.lp.click_sign_in_button()
         element= sb.get_element(sign_in_success_locator)
         assert element.is_displayed()
         #assert element.text.__contains__(sign_in_displayed_message)
-    """
+
     def test_login_with_invalid_email_valid_password(self):
         re = RandomEmail()
-        lp = LoginPage(self.driver)
-        lp.click_sign_in()
-        lp.enter_value_to_email_field(self.re.random_email())
-        lp.enter_value_to_password_field(valid_password)
-        lp.click_sign_in_button()
-        element = lp.invalid_email_message()
+        self.lp.click_sign_in()
+        self.lp.enter_value_to_email_field(self.re.random_email())
+        self.lp.enter_value_to_password_field(valid_password)
+        self.lp.click_sign_in_button()
+        element = self.lp.invalid_email_message()
         assert element.is_displayed()
-    """
+
     def test_login_with_valid_email_invalid_password(self):
-        lp = LoginPage(self.driver)
-        lp.click_sign_in()
-        lp.enter_value_to_email_field(valid_email)
-        lp.enter_value_to_password_field(invalid_password)
-        lp.click_sign_in_button()
+        self.lp.click_sign_in()
+        self.lp.enter_value_to_email_field(valid_email)
+        self.lp.enter_value_to_password_field(invalid_password)
+        self.lp.click_sign_in_button()
         #element = sb.get_element(invalid_email_password_error_message_locator)
-        element = lp.invalid_email_message()
+        element = self.lp.invalid_email_message()
         assert element.is_displayed()
     
 
     def test_login_with_no_credentials(self):
-        lp = LoginPage(self.driver)
-        lp.click_sign_in()
-        lp.enter_value_to_email_field("")
-        lp.enter_value_to_password_field("")
-        lp.click_sign_in_button()
-        element = lp.empty_email_field()
+        self.lp.click_sign_in()
+        self.lp.enter_value_to_email_field("")
+        self.lp.enter_value_to_password_field("")
+        self.lp.click_sign_in_button()
+        element = self.lp.empty_email_field()
         assert element.text.__contains__(no_credential_expected_warning_message)
-        element1 = lp.empty_password_field()
+        element1 = self.lp.empty_password_field()
         assert element1.is_displayed()
-    """
+
 
