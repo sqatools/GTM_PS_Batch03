71c8d32f58d0df6783e5de09b851090f22f796f9
ashokpusarla002 <128246368+ashokpusarla002@users.noreply.github.com>
2024-06-24 00:44:41 +0530

    Delete Nithulya directory

--- Nithulya/SeleniumCode/PytestFrameWork/frameworkPractice2/test_fixture2.py
+++
@@ -1,8 +0,0 @@
-
-def test_login_with_valid_credential():
-    print('Testing test_login_with_valid_credential')
-
-def test_login_with_valid_email_and_invalid_password():
-    print('Testing test_login_with_valid_email_and_invalid_password ')
-
-
