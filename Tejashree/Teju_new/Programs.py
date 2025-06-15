7d925b1ebb35f09c5d5097b9dd2ed2cb500e894e
Tejashree <tejashreebannure@gmail.com>
2025-06-15 14:42:37 +0530

    new file5

---
+++ Tejashree/Teju_new/Programs.py
@@ -0,0 +1,41 @@
+# 1). Python Program to add two integer values.
+# 2). Python Program to subtract two integer values.
+# 3). Python program to multiply two numbers.
+"""
+a = 10
+b = 20
+print("addition of a and b:", a+b)
+print("subtraction of a and b:", a-b)
+print("multiply of a and b:", a*b)
+
+# 4). Python program to repeat a given string 5 times.
+# Input :
+# str1 = “SQATools”
+# Output :
+# “SQAToolsSQAToolsSQAToolsSQAToolsSQATools”
+
+str1 = "SQATools"
+n1=5
+for i in range(7):
+    print(str1[i],end="")
+
+print("output:", str1*n1)
+
+#5). Python program to get the Average of given numbers.
+# Formula: sum of all the number/ total number
+Input:
+a = 40
+b = 50
+c = 30
+Output :
+Average = 40
+a = 40
+b = 50
+c = 30
+print("average of given number:", (a+b+c)/3)
+"""
+
+
+
+
+
